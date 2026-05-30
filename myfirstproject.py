39a7534907b594db596544da6e55c53310c09d1a
drabdo1990 <80887685+drabdo1990@users.noreply.github.com>
2026-05-30 17:33:59 +0300

    commit directly to the main braanch
    my first project

---
+++ myfirstproject.py
@@ -0,0 +1,6 @@
+print ("welcome to the youtube channel name generator:")
+nickname = input ("what is your nickname?\n")
+channel_type = input("what is your channel about?\n")
+print ("you could name your channel" + "(" + channel_type + " with " + nickname + ")" )
+
+
