3807db10c18ea3f0edceb7314de602d1e5e19f62
drabdo1990 <80887685+drabdo1990@users.noreply.github.com>
2026-06-01 23:57:12 +0300

    commit directly to the main branch

---
+++ third project.py
@@ -0,0 +1,7 @@
+str_length = input("please enter the length \n")
+str_width = input ("please enter the width \n")
+length = float (str_length)
+width = float (str_width)
+area = length * width
+str_area = str(area)
+print (" your area is " + str_area)
